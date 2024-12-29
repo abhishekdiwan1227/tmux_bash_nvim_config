bd48bc5c01ed98ffef0b64c9cf8032703ec7af22
Abhishek Diwan <abhishekdiwan1227@gmail.com>
2024-12-29 11:47:49 +0530

    avocado automated checkin

--- nvim/lua/langsupport/init.lua
+++ nvim/lua/langsupport/init.lua
@@ -1,14 +1,4 @@
-vim.api.nvim_create_autocmd('FileType', {
-	pattern = 'sh',
-	callback = function()
-		vim.lsp.start({
-			name = 'bash-language-server',
-			cmd = { 'bash-language-server', 'start' },
-		})
-	end,
-})
-
 local lspconfig = require("lspconfig")
 lspconfig.lua_ls.setup {}
 lspconfig.gopls.setup {}
-
+lspconfig.bashls.setup {}
